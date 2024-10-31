9d3f8e73b51c57665a811940b9c6ad5d11e4aa55
Miguel Kim Davis <miguelkimdavis0957@gmail.com>
2024-10-31 13:50:01 +0300

    commit1

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, inject } from '@angular/core';
+//
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -7,60 +8,52 @@ import { SignupDetailsService } from '../services/signupdetails.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
-  // authservice:AuthService = inject(AuthService);
   isLoading: boolean = false;
   isSignupMode: boolean = true;
 
-  onSignupSwitchMode(){
+  onSignupSwitchMode() {
     this.isSignupMode = !this.isSignupMode;
   }
 
-  onSignupFormSubmit(form:NgForm){
+  onSignupFormSubmit(form: NgForm) {
     this.isLoading = true;
     const name = form.value.name;
     const registrationNumber = form.value.registrationNumber;
     const email = form.value.email;
     const password = form.value.password;
 
-    console.log(form.value);
-
-
-    if(this.isSignupMode){
-      this.isLoading = true;
-      this.authservice.signup(email, password)
-      .subscribe({
-        next:(res)=>{
-          console.log(res);
-          this.isLoading = false;
+    if (this.isSignupMode) {
+      this.authservice.signup(email, password).subscribe({
+        next: (authResponse) => {
+          this.signupdetailsservice
+            .usersignupdetails(name, registrationNumber, email, password)
+            .subscribe({
+              next: () => {
+                this.isLoading = false;
+                form.reset();
+              },
+              error: (err) => {
+                alert(err.message);
+                this.isLoading = false;
+              },
+            });
         },
-        error:(err)=>{
+        error: (err) => {
           alert(err.message);
           this.isLoading = false;
-        }
-      })
+        },
+      });
     }
-
-    this.signupdetailsservice.usersignupdetails(name, registrationNumber, email, password)
-    .subscribe({
-      next:(res)=>{
-        console.log(res);
-      },
-      error:(err)=>{
-        alert(err.message);
-      }
-    })
-
-
-    form.reset();
   }
 
-  constructor(private router: Router,private authservice:AuthService,private signupdetailsservice: SignupDetailsService) { }
+  constructor(
+    private router: Router,
+    private authservice: AuthService,
+    private signupdetailsservice: SignupDetailsService
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
